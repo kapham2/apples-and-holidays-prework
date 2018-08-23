2fbe4a4da1541ed379d2269e89f45ec0eaf67cd3
Kim Pham <pham.kim.a@gmail.com>
2018-08-23 23:35:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     holiday.each do |holi, supply|
-      if supply == "BBQ"
+      if supply. == "BBQ"
         holiday_array.push(holi)
       end
     end
